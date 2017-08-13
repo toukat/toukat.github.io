1fef0c256c6d1397771b8d555f3648239939b394
Touka Tsuwamono <excellentnuke@gmail.com>
2017-08-13 14:59:39 -0700

    Initial commit

---
+++ themes/ruri-dark/mascots.js
@@ -0,0 +1,6 @@
+var mascotList = [ "ruri1.png", "ruri3.png", "chibi-haruna.png",
+   "eri.png", "kosaki.png", "kuroneko.png", "maki.png",
+   "yozora.png", "anastasia.png", "jougasaki_mika.png",
+   "ranko.png", "nao1.png", "alisa.png", "reina1.png",
+   "maki2.png", "crab1.png","tsugumi1.png", "tsugumi2.png", "sinon2.png",
+   "shibuyarin1.png", "makoto.png" ];
